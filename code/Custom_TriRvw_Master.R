c8f4c7be73104fedaee450552a3b3028914eb068
cfelletter <cfelletter@usbr.gov>
2020-03-19 16:36:12 -0600

    Push before I start deleting this to make Custom_TriRvw_Master.R for April MTG only

--- code/Custom_TriRvw_Master.R
+++ code/Custom_TriRvw_Master.R
@@ -63,58 +63,6 @@ endyr <- 2040 #60
 
 # # ##########################################################################
 
-# # # #Compare 2017 and 2020 Feb MTG Scenarios
-# scens <- list(
-#   "TriRvw17 Limited Funding 1.66M Tons" = "DNF,Jan2017,WQIP_Senario3",
-#   "Feb2020 Plan of Implemenation 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113"
-# )
-# 
-# #### Compare Models for Feb WORKGROUP ####
-# 
-# #file names
-# Model.Step.Name <- Figs <- "TriRvw17_vs_Feb2020" #plot title and results/folder name #[plot type] identifying name .pdf
-# customcolorltpt <- F
-# startyr <- 2017 #filter out all years > this year
-# 
-# 
-  
-# # ##########################################################################
-
-# # # Feb MTG Hydrologies
-scens <- list(
-  "ISM 1906-2017" = "2007Dems,MTOM_Most,DNF with Salinity,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113",
-  "ISM 1931-2017" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113"
-)
-
-
-#file names
-Model.Step.Name <- Figs <- "Feb2020_HydroCompare" #plot title and results/folder name #[plot type] identifying name .pdf
-customcolorltpt <- F
-startyr <- 2020 #filter out all years > this year
-
-
-# # ##########################################################################
-
-#### Updated figure types to send to WORKGROUP before finalize ####
-
-#file names
-Model.Step.Name <- Figs <- "Draft_FigUpdates" #plot title and results/folder name
-customcolorltpt <- F
-startyr <- 2020 #filter out all years > this year
-
-
-scens <- list(
-  "Scenario 1 - 1.31M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario1_2020_20200113",
-  "Scenario 2 - 1.58M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario2_2020_20200113",
-  "Scenario 3 - 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113"#,
-  # "No Additional Beyond 2020 - 1.31M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario1_2020_20200113",
-  # "2019 Funding Levels - 1.58M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario2_2020_20200113",
-  # "Plan of Implementation - 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113"#,
-  # # "Max Potential Controls 2.34M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario4_2020_20200113"
-)
-
-# # ##########################################################################
-
 # #### Update to Jan 2020 CRSS and 2018 NFS - Not for Feb MTG####
 # 
 # #file names
@@ -163,6 +111,117 @@ scens <- list(
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+#### ParadoxOnOffRamp ####
+
+# #file names
+# Model.Step.Name <- Figs <- "ParadoxOnOffRamp" #plot title and results/folder name
+# customcolorltpt <- F
+# startyr <- 2020 #filter out all years > this year
+# 
+# 
+# scens <- list(
+#   "Paradox On100k" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_Paradox100",
+#   "Padadox Off" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxOff",
+#   "Paradox Off20Half2126100k27" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp"#,
+#   # "No Additional Beyond 2020 - 1.31M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario1_2020_20200113",
+#   # "2019 Funding Levels - 1.58M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario2_2020_20200113",
+#   # "Plan of Implementation - 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario3_2020_20200113"#,
+#   # # "Max Potential Controls 2.34M Tons" = "2007Dems,MTOM_Most,DNF with Salinity Binational,Nov2019_9007,IG_DCP_4.3.0,WQIP_Scenario4_2020_20200113"
+# )
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# #### VIC Test ####
+# 
+# #file names
+# Model.Step.Name <- Figs <- "VIC" #plot title and results/folder name
+# customcolorltpt <- F
+# startyr <- 2020 #filter out all years > this year
+# 
+# 
+# scens <- list(
+#   "ISM 1906-2017" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp",
+#   "VIC" = "2007Dems,MTOM_Most,VIC,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp" #this is 1971 to 2018 reg 
+#   #### REGenerate salt input files were bad again - verify no INF in GainsAboveLF Salt
+# )
+
+# # ##########################################################################
+
+# # #Compare 2017 and 2020 April MTG Scenarios
+scens <- list(
+  "TriRvw17 Limited Funding 1.66M Tons" = "DNF,Jan2017,WQIP_Senario3",
+  "Feb2020 Plan of Implemenation 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp"
+)
+
+#### Compare Models for Feb WORKGROUP ####
+
+#file names
+Model.Step.Name <- Figs <- "TriRvw17_vs_Apr2020" #plot title and results/folder name #[plot type] identifying name .pdf
+customcolorltpt <- F
+startyr <- 2017 #filter out all years > this year
+
+
+
+# # ##########################################################################
+
+# # # Feb MTG Hydrologies
+scens <- list(
+   "ISM 1906-2018" = "2007Dems,MTOM_Most,DNF with Salinity 19062018 Reg8918,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp",
+  "ISM 1931-2018" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp"
+)
+
+
+#file names
+Model.Step.Name <- Figs <- "HydroCompare" #plot title and results/folder name #[plot type] identifying name .pdf
+customcolorltpt <- F
+startyr <- 2020 #filter out all years > this year
+
+
+# # ##########################################################################
+
+#### Updated figure for April 2020 meeting ####
+
+#file names
+Model.Step.Name <- Figs <- "Draft_FigUpdates_3Fig" #plot title and results/folder name
+customcolorltpt <- F
+startyr <- 2020 #filter out all years > this year
+
+
+scens <- list(
+  "Scenario 1 - 1.31M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario1_2020_20200113",
+  "Scenario 2 - 1.58M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario2_2020_20200113_ParadoxRamp",
+  "Scenario 3 - 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp"#,
+  # "Scenario 4 - 2.34M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario4_2020_20200113_ParadoxRamp"#,
+)
+
+
+ #### Updated figure for April 2020 meeting ####
+
+#file names
+Model.Step.Name <- Figs <- "Draft_FigUpdates_4Fig" #plot title and results/folder name
+customcolorltpt <- F
+startyr <- 2020 #filter out all years > this year
+
+scens <- list(
+  "Scenario 1 - 1.31M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario1_2020_20200113",
+  "Scenario 2 - 1.58M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario2_2020_20200113_ParadoxRamp",
+  "Scenario 3 - 1.69M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp",
+  "Scenario 4 - 2.34M Tons" = "2007Dems,MTOM_Most,DNF with Salinity 19312018,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario4_2020_20200113_ParadoxRamp"#,
+)
+
+# # ##########################################################################
+
+#file names
+Model.Step.Name <- Figs <- "Check_Feb_Dist" #plot title and results/folder name
+customcolorltpt <- F
+startyr <- 2021 #filter out all years > this year
+
+scens <- list(
+  "Jan19 ISM 1906-2018" = "2007Dems,MTOM_Most,DNF with Salinity 19062018 Reg8918,Jan2020_RW8,IG_DCP_4.4.0_RW8,WQIP_Scenario3_2020_20200113_ParadoxRamp",
+  "Feb19 Release" = "Feb2020_2021_4.4.1,DNF with Salinity,2007Dems,IG_DCP,MTOM_Most"
+)
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ### Custom Color/Pt Types/Scales ### 
 
 #### Paired Scens #### - 4 colors, first scen is dashed (old), second solid
@@ -236,32 +295,30 @@ source("code/Custom_WQAnn_CloudswHist_woTitles.R") #clouds for Review, no title 
 
 
 #MinMax must be run after a code has created scen_res but could work any scen_res
-source("code/MinMax_Check_NoFct.R")
+source("code/MinMax_Check_NoFct.R") #RUNS REALLY SLOW WITH MONTHLY so only use with WQAnn
 
 #SaltMassBal
 source("code/Custom_MassBalAnn.R")
 #Doesn't include UB Salt Mass Balance.ExportSaltMassExtra since this needs to be recreated in CRSS.OFFC
 # source("code/Custom_MassBalAnn_OldControl.R")
-# #MinMax must be run after a code has created scen_res but could work any scen_res
-# source("code/MinMax_Check_NoFct.R")
 
 #FlowMassBal
 source("code/Custom_FlowBalAnn.R")
 
 # #This script can only be used with data processed using the new output.control file 6/27/2019
-# # source("code/Custom_PowellMead_3Panel.R") #still need to fix issues Jim suggested 
-# #need to add AnnualSalinity.PowellInflow_FWAAC to Nov 2019 model to use new (also add to WQAnn.rdf in output.control)
-# source("code/Custom_PowellMead_3Panel_OldControlFile.R") 
+source("code/Custom_PowellMead_3Panel.R") #still need to fix issues Jim suggested - Jan2020 model now has Pow/Mead Inflow FWAAC
+#need to add AnnualSalinity.PowellInflow_FWAAC to Nov 2019 model to use new (also add to WQAnn.rdf in output.control)
+# source("code/Custom_PowellMead_3Panel_OldControlFile.R") #using this Jan 2020 model this shouldn't be an issue any more
 # 
 # 
 # #Saltstorage
-# source("code/Custom_SaltStorage.R")
+source("code/Custom_SaltStorage.R")
 # 
 # # Powell Mead PE
-# source("code/Custom_ResPE.R")
+source("code/Custom_ResPE.R")
 # source("code/Custom_PE_CloudswHist.R") #clouds for PE - UNDER DEVELOPMENT
-# source("code/MinMax_Check_NoFct.R")
-# 
+source("code/MinMax_Check_NoFct.R") #RUNS REALLY SLOW WITH MONTHLY so only use with WQAnn
+
 
 
 
