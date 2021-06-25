79cd7b8cbf9664e5936c493c481c3a3697d5835e
cfelletter <cfelletter@usbr.gov>
2021-06-25 07:02:59 -0600

    arrow fix

--- code/R Process Tools/get_CRSS_data_make_feather_UB.R
+++ code/R Process Tools/get_CRSS_data_make_feather_UB.R
@@ -4,6 +4,8 @@ library(feather)
 
 rwd <- read_rwd_agg("C:/Users/fellette/Documents/GIT/RW-RDF-Process-Plot/rw_agg/rw_agg_CRSPops.csv")
 
+CRSSDIR <- Sys.getenv("CRSS_DIR")
+
 ## previous offical run #
 previous_scens <- rw_scen_gen_names("Apr2021_2022.v5.1", c("DNF", "ISM1988_2019"),
                                     "2016Dems,IG_DCP.v5.1", 
@@ -29,8 +31,6 @@ scens_latest_ST <- rw_scen_gen_names("Jun2021_2022", c("ISM1988_2019"),
                                   paste0("Trace", 4:38))
 scens_latest
 
-# rw_scen_aggregate(scens_latest, agg = rwd, scen_dir = "M:/Shared/CRSS/2021/Scenario", 
-#                   file = "crsp_ops_data_latest.feather")
 
 ## no DRO run #
 scens_noUBDRO <- rw_scen_gen_names("Jun2021_2022", c("DNF", "ISM1988_2019"),
@@ -45,16 +45,19 @@ scens_noUBDRO_ST <- rw_scen_gen_names("Jun2021_2022", c("DNF", "ISM1988_2019"),
                                    "2016Dems,IG_DCPnoUBDRO", 
                                    paste0("Trace", 4:38))
 
-scens <- c(scens_latest,scens_noUBDRO) #combine
+scens <- c(previous_scens,scens_latest,scens_noUBDRO) #combine
 
-#combine prv
+# # aggregate the combined scens list 
+# rw_scen_aggregate(scens_latest, agg = rwd, scen_dir = "M:/Shared/CRSS/2021/Scenario", 
+#                   file = "crsp_ops_data_latest.feather")
 
-zz <- feather::read_feather("crsp_ops_data_prv.feather") 
-head(zz)  
-zz2 <- feather::read_feather("crsp_ops_data.feather") 
+
+zz1 <- feather::read_feather(file.path(CRSSDIR,"crsp_ops_data_prv.feather")) 
+head(zz1)  
+zz2 <- feather::read_feather(file.path(CRSSDIR,"crsp_ops_data_cur.feather")) 
 head(zz2)  
 
-zz <- rbind.data.frame(zz,zz2)
+zz <- rbind.data.frame(zz1,zz2)
 summary(zz)
 
 zz <- zz %>%
@@ -74,7 +77,13 @@ unique(zz$Variable)
 length(unique(zz$Scenario))
 
 
-feather::write_feather(zz,"crsp_ops_data.feather") 
+arrow::write_feather(zz,file.path(CRSSDIR,"crsp_ops_data.feather")) 
 
-zz_check <- feather::read_feather("crsp_ops_data.feather") 
+zz_check <- arrow::read_feather("C:/Users/fellette/Documents/GIT/CRSS/crsp_ops_data.feather") 
+summary(zz_check)
+unique(zz_check$ScenarioGroup)
+unique(zz_check$Variable)
+length(unique(zz_check$Scenario))
 
+# Error in coldataFeather(x, i) : 
+#   embedded nul in string: 'Apr2021_2022.v5.1,DNF,2016Dems,I\0\0\0\0\v\0\0\0\026\0\0\0!\0\0\0,\0'
