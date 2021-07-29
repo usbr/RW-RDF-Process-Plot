6318f24b5e7222b3a18aa73448b068b88d97ff2d
cfelletter <cfelletter@usbr.gov>
2021-07-28 18:28:53 -0600

    fixed 24MS counting issue

--- code/energyCRSP_blw3490_WY.R
+++ code/energyCRSP_blw3490_WY.R
@@ -69,10 +69,12 @@ for (i in 1:length(unique(crss_energy$TraceNumber))) {
   }
 }
 
-unique(df_ond$TraceNumber)
-unique(df_ond$ScenarioGroup)
-names(df_ond)
-names(crss_energy)
+#### MUST FILTER OUT 24MS after EOCY 2021!!!! ##### 
+df_ond <- df_ond %>% filter(Date <= "2021-12-31")
+
+head(df_ond)
+unique(df_ond$water_year)
+unique(df_ond$Date)
 
 #combine crss energy with 24MS Oct-Dec energy 
 energy_all <- bind_rows(crss_energy[,names(df_ond)], df_ond) #%>% # crss %>%
@@ -90,125 +92,30 @@ df <- df %>%pivot_wider(names_from= Variable,values_from= Value) %>%
 #CRSP Energy Generation when Powell < 3490  
 df %>% filter(water_year <= 2026,water_year >= 2022,Powell.PE <= 3490) %>%
   dplyr::group_by(water_year,ScenarioGroup,TraceNumber) %>%
-  summarise(CRSP.Energy = sum(CRSP.Energy))  %>%
-    ggplot(aes(x=factor(water_year),y=CRSP.Energy,color=ScenarioGroup)) +
-    geom_boxplot() +
-  ylim(0,10000) +
+  summarise(CRSP.Energy = sum(CRSP.Energy)) %>%
+  # report out
+  dplyr::group_by(water_year,ScenarioGroup) %>%
+  summarise(mean = mean(CRSP.Energy),med = median(CRSP.Energy),max = max(CRSP.Energy),min = min(CRSP.Energy)) %>%
+  write.csv(file = file.path(figures_dir,paste0("CRSPEnergy3490_WY_plotstats.csv")))
+  # plot 
+  ggplot(aes(x=factor(water_year),y=CRSP.Energy,color=ScenarioGroup)) +
+  geom_boxplot() +
+  ylim(0,7500) +
   labs(x = "WY Year", y = "Energy (GWH/yr)" ,title = "CRSP Energy Generation when Powell < 3490") 
 ggsave(filename = file.path(figures_dir,paste0("CRSPEnergyPow3490_WY_2226.png")), width = ww,height = hh)#width= width, height= height)
 
 # CRSP Energy Generation All Traces
 df %>%   filter(water_year <= 2026,water_year >= 2022) %>%
   dplyr::group_by(water_year,ScenarioGroup,TraceNumber) %>%
-  summarise(CRSP.Energy = sum(CRSP.Energy))  %>%
+  summarise(CRSP.Energy = sum(CRSP.Energy)) %>%
+  ## report out
+  # dplyr::group_by(water_year,ScenarioGroup) %>%
+  # summarise(mean = mean(CRSP.Energy),med = median(CRSP.Energy),max = max(CRSP.Energy),min = min(CRSP.Energy)) %>%
+  # write.csv(file = file.path(figures_dir,paste0("CRSPEnergy_WY_plotstats.csv")))
+  # plot 
   ggplot(aes(x=factor(water_year),y=CRSP.Energy,color=ScenarioGroup)) +
   geom_boxplot() +
-  ylim(0,10000) +
+  ylim(0,7500) +
   labs(x = "WY Year", y = "Energy (GWH/yr)" ,title = "CRSP Energy Generation All Traces") 
 ggsave(filename = file.path(figures_dir,paste0("CRSPEnergy_WY_alltraces_2226.png")), width = ww,height = hh)#width= width, height= height)
-
-
-#### =============== CHECK DATA =============== ####
-#get WY energy for all reservoirs to check 
-df_long <- df %>% 
-  pivot_longer(cols = Powell.PE:CRSP.Energy, names_to= "Variable",values_to= "Value") %>% 
-  filter(water_year <= 2026,water_year >= 2022) %>%
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber,Variable) %>%
-  summarise(Value = sum(Value))  %>%
-  pivot_wider(names_from= Variable,values_from= Value) 
-write.csv(df_long,file = file.path(figures_dir,paste0("raw_CRSPEnergy_WY.csv")))
-
-#get outflow data and compute WY totals
-crss_out <- feather::read_feather(file.path(feather_data_dir,"crspopsdata.feather"))
-# unique(crss_out$Variable)
-out_slots <- c("Powell.Outflow","BlueMesa.Outflow","FlamingGorge.Outflow","Powell.PE")
-crss_out <- filter(crss_out, Variable %in% out_slots) %>%
-  mutate(
-    Date = ymd(paste(Year, match(Month, month.name), "01", sep = "-")), 
-    water_year = date_to_wy(Date)) %>%
-  select(-Year, -Month) %>%
-  pivot_wider(names_from= Variable,values_from= Value) %>% 
-  # filter(Powell.PE <= 3490) %>%
-  # mutate(CRSP.Outflow = Powell.Outflow+BlueMesa.Outflow+FlamingGorge.Outflow) %>%
-  filter(water_year <= 2026,water_year >= 2022) #%>%
-
-#sum acrossed wy and dump data 
-crss_out %>% 
-  #must filter Powell PE < 3490 before this step 
-  pivot_longer(cols = FlamingGorge.Outflow:Powell.Outflow, names_to= "Variable",values_to= "Value") %>% 
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber,Variable) %>%
-  summarise(Value = sum(Value))  %>%
-  pivot_wider(names_from= Variable,values_from= Value) %>%
-  write.csv(file = file.path(figures_dir,paste0("raw_CRSPoutflow_WY.csv")))
-
-##Powell WY outflow when Powell < 3490  
-crss_wy_blw3490 <- crss_out %>% filter(Powell.PE <= 3490) %>%
-  #must filter Powell PE < 3490 before this step 
-  pivot_longer(cols = FlamingGorge.Outflow:Powell.Outflow, names_to= "Variable",values_to= "Value") %>% 
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber,Variable) %>%
-  summarise(Value = sum(Value)/1000)  %>%
-  pivot_wider(names_from= Variable,values_from= Value) # %>%
-
-title <- "Powell WY Annual Outflow when Powell below 3490"
-crss_wy_blw3490 %>%
-  ggplot(aes(x=factor(water_year),y=Powell.Outflow,color=ScenarioGroup)) +
-  geom_boxplot() +
-  labs(title=title, y="Outflow (kaf/yr)")
-ggsave(filename = file.path(figures_dir,paste0("Powell_WY_Out_3490.png")), width = ww,height = hh)#width= width, height= height)
-
-title <- "Flaming Gorge WY Annual Outflow when Powell below 3490"
-crss_wy_blw3490 %>%
-  ggplot(aes(x=factor(water_year),y=FlamingGorge.Outflow,color=ScenarioGroup)) +
-  geom_boxplot() +
-  labs(title=title, y="Outflow (kaf/yr)")
-ggsave(filename = file.path(figures_dir,paste0("FlamingGorge_WY_Out_3490.png")), width = ww,height = hh)#width= width, height= height)
-
-##Powell WY outflow all traces 
-crss_wy_all <- crss_out %>% 
-  pivot_longer(cols = FlamingGorge.Outflow:Powell.Outflow, names_to= "Variable",values_to= "Value") %>% 
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber,Variable) %>%
-  summarise(Value = sum(Value)/1000)  %>%
-  pivot_wider(names_from= Variable,values_from= Value) # %>%
-
-title <- "Powell WY Annual Outflow all traces"
-crss_wy_all %>%
-  ggplot(aes(x=factor(water_year),y=Powell.Outflow,color=ScenarioGroup)) +
-  geom_boxplot() +
-  labs(title=title, y="Outflow (kaf/yr)")
-ggsave(filename = file.path(figures_dir,paste0("Powell_WY_Out_alltr.png")), width = ww,height = hh)#width= width, height= height)
-
-title <- "Flaming Gorge WY Annual Outflow all traces"
-crss_wy_all %>%
-  ggplot(aes(x=factor(water_year),y=FlamingGorge.Outflow,color=ScenarioGroup)) +
-  geom_boxplot() +
-  labs(title=title, y="Outflow (kaf/yr)")
-ggsave(filename = file.path(figures_dir,paste0("FlamingGorge_WY_Out_alltr.png")), width = ww,height = hh)#width= width, height= height)
-
-#### summary data used in plots 
-
-# CRSP Energy Generation All Traces
-df %>%   filter(water_year <= 2026,water_year >= 2022) %>%
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber) %>%
-  summarise(CRSP.Energy = sum(CRSP.Energy))  %>%
-  ggplot(aes(x=factor(water_year),y=CRSP.Energy,color=ScenarioGroup)) +
-  geom_boxplot() +
-  ylim(0,10000) +
-  labs(x = "WY Year", y = "Energy (GWH/yr)" ,title = "CRSP Energy Generation All Traces") 
-ggsave(filename = file.path(figures_dir,paste0("CRSPEnergy_WY_alltraces_2226.png")), width = ww,height = hh)#width= width, height= height)
-
-
-#report out numbers
-#CRSP Energy Generation when Powell < 3490
-dat <- df %>% filter(water_year <= 2026,Powell.PE <= 3490) %>%
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber) %>%
-  summarise(CRSP.Energy = sum(CRSP.Energy))  #%>%
-  # summarise(Med = median(CRSP.Energy),Mean = mean(CRSP.Energy),Max = max(CRSP.Energy),Min = min(CRSP.Energy))
-write.csv(dat,file = file.path(figures_dir,paste0("CRSPEnergyPow3490_WY.csv")))
-
-# CRSP Energy Generation All Traces
-dat <- df %>% filter(water_year <= 2026) %>%
-  dplyr::group_by(water_year,ScenarioGroup,TraceNumber) %>%
-  summarise(CRSP.Energy = sum(CRSP.Energy))  #%>%
-  # summarise(Med = median(CRSP.Energy),Mean = mean(CRSP.Energy),Max = max(CRSP.Energy),Min = min(CRSP.Energy))
-write.csv(dat,file = file.path(figures_dir,paste0("CRSPEnergyAllTraces.csv")))
 
