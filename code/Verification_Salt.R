aab372a6c725bc3c2b250ef8b584d03ca13b0f93
cfelletter <cfelletter@usbr.gov>
2022-01-18 09:31:47 -0700

    Verification_Salt.R now processes and plots monthly 3 panel graphics

--- code/Verification_Salt.R
+++ code/Verification_Salt.R
@@ -64,16 +64,12 @@ if (!file.exists(fig_dir) | !file.exists(data_dir)) {
 #### #### A. Read flow data  ####  ####
 ################################################################################
 
-#agg file specifying which slots
-rw_agg_file <- "SaltVerificationRun_rwagg.csv" 
-#read agg file specifying which slots
+#do this with rwagg becasue you need different FWAAC slots 
+rw_agg_file <- "SaltVerificationAnn_rwagg.csv" 
 rwa1 <- rwd_agg(read.csv(file.path(getwd(),"rw_agg", rw_agg_file), stringsAsFactors = FALSE)) #ubres.rdf res.rdf
-
-#rw_scen_aggregate() will aggregate and summarize multiple scenarios, essentially calling rdf_aggregate() for each scenario. Similar to rdf_aggregate() it relies on a user specified rwd_agg object to know how to summarize and process the scenarios.
 df_annual <- rdf_aggregate(rwa1, rdf_dir = file_dir) #MUST be in Scenario FOLDER! not results folder
 
-#fix issues with class
-df_annual <- as.data.frame(df_annual)
+df_annual <- as.data.frame(df_annual) #fix issues with class #i have to do this to get it to work, not sure why 
 
 nodes <- c('glen','cameo','gunn','dolor','cisco','grwy','gdale','yampa','duch',
            'white','grut','sanraf','arch','bluf','lees','grcan','virgin','hoover',
@@ -81,14 +77,32 @@ nodes <- c('glen','cameo','gunn','dolor','cisco','grwy','gdale','yampa','duch',
 nodenums <- c(1,2,6:8,10:12,14:20,23:25,28,29)
 flownm <- paste0(nodenums,rep("_flow_",length(nodes)),nodes)
 massnm <- paste0(nodenums,rep("_mass_",length(nodes)),nodes)
-concnm <- paste0(nodenums,rep("_FWAAC_",length(nodes)),nodes)
+concnm <- paste0(nodenums,rep("_conc_",length(nodes)),nodes)
 
-df_obs <- readxl::read_xlsx('data/HistFlowMassConc.xlsx',col_names = T, ) 
+df_obs <- readxl::read_xlsx('data/HistFlowMassConcAnn.xlsx',col_names = T, ) 
 df_obs = df_obs %>% pivot_longer(cols=names(df_obs)[3:62],names_to = 'Variable',values_to = 'Value')
 
 df_annual$DataType = rep("Sim",times=dim(df_annual)[1])
 
 df_annual <- rbind.data.frame(df_annual[,names(df_obs)],df_obs)
+
+#### monthly ####
+
+rw_agg_file <- "SaltVerificationMon_rwagg.csv" 
+rwa1 <- rwd_agg(read.csv(file.path(getwd(),"rw_agg", rw_agg_file), stringsAsFactors = FALSE)) #ubres.rdf res.rdf
+df_monthly <- rdf_aggregate(rwa1, rdf_dir = file_dir) #MUST be in Scenario FOLDER! not results folder
+#get everything on a date 
+df_monthly$Date = as.Date(paste0(df_monthly$Year,df_monthly$Month,"01"), format = "%Y%B%d")
+#get a numeric month number
+df_monthly$MonthNum = as.numeric(format.Date(df_monthly$Date, format = "%m"))
+
+df_monthly$DataType = rep("Sim",times=dim(df_monthly)[1])
+
+df_monthly_obs <- readxl::read_xlsx('data/HistFlowMassConcMonthly.xlsx',col_names = T, ) 
+df_monthly_obs = df_monthly_obs %>% pivot_longer(cols=names(df_monthly_obs)[3:62],names_to = 'Variable',values_to = 'Value')
+
+df_monthly <- rbind.data.frame(df_monthly[,names(df_monthly_obs)],df_monthly_obs)
+
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 4. Plot Node Figures 
@@ -106,9 +120,9 @@ for (i in 1:length(nodes)) {
   #     dir.create(file.path(file_dir,nodes[i]))
   #   }
   # }
-  
+
   print(paste("Node",nodes[i]))
-  
+
   # 3 panel annual plot
   p1 <- df_annual %>%
     dplyr::filter(Variable == flownm[i])  %>%
@@ -116,45 +130,43 @@ for (i in 1:length(nodes)) {
     mutate(Value = Value/1000) %>%
     ggplot(aes(x = Year, y = Value, color = DataType)) +
     geom_line() +
-    theme_light() + 
+    theme_light() +
     theme(axis.title.x = element_blank()) +
     labs(title = paste(nodes[i]), y = "Flow (KAF/yr)",x="")
   # print(p1)
-  
+
   p2 <- df_annual %>%
     dplyr::filter(Variable == massnm[i])  %>%
     group_by(DataType,Variable,Year) %>%
     mutate(Value = Value/1000) %>%
     ggplot(aes(x = Year, y = Value, color = DataType)) +
     geom_line() +
-    theme_light() + 
-    # theme(legend.position = "none", axis.title.x = element_blank()) +
+    theme_light() +
     theme(axis.title.x = element_blank()) +
-    
-    labs(y = "Mass (ktons)",x="")
+    labs(y = "Mass (ktons/yr)",x="")
   # print(p2)
-  
+
   p3 <- df_annual %>%
     dplyr::filter(Variable == concnm[i])  %>%
     group_by(DataType,Variable,Year) %>%
     ggplot(aes(x = Year, y = Value, color = DataType)) +
     geom_line() +
-    theme_light() + 
+    theme_light() +
     theme(axis.title.x = element_blank()) +
     labs(y = "FWAAC (mg/l)",x="")
   # print(p3)
-  
+
   grid.arrange(p1,p2,p3,ncol=1)
-  
+
   if(printfigs==T){ ggsave(filename = file.path(fig_dir,paste(nodenums[i],nodes[i],"AnnPlot",scens,".png")), width = gage_widths[1],height = gage_heights[1])}
-  
+
   #calculate residual
   gage <- df_annual %>%
     dplyr::filter(Variable == massnm[i] & DataType == "Obs")
 
   simulated <- df_annual %>%
     dplyr::filter(Variable == massnm[i] & DataType == "Sim")
-  
+
   diff <- gage
   diff$Value = simulated$Value - gage$Value
   diff$Variable = rep("Residual",times = length(diff$Variable))
@@ -162,52 +174,54 @@ for (i in 1:length(nodes)) {
   df_csv <- df_annual %>%
     dplyr::filter(Variable == flownm[i] | Variable == massnm[i] | Variable == concnm[i])  %>%
     pivot_wider(names_from = Variable,values_from = Value)
-  #dump out the data 
+  #dump out the data
   if(printfigs==T){ write.csv(x = df_csv,
                               file = file.path(data_dir,paste(nodenums[i],nodes[i],"Data",scens,".csv")))}
 
   # annual residual
   diff <- diff %>%
+    mutate(Value = Value/1000) %>%
     dplyr::group_by(Year, Variable)
   r1 <- diff %>%
     ggplot(aes(x = Year, y = Value, color = Variable)) +
     geom_line() +
     theme_light() +
     theme(legend.position = "none", axis.title.x = element_blank()) +
-    geom_hline(yintercept = 0) + 
+    geom_hline(yintercept = 0) +
     scale_y_continuous(labels = scales::comma) +
-    labs(title = paste(nodes[i],"Annual Mass Residual"), y = "Mass (tons/yr)")
+    labs(title = paste(nodes[i],"Annual Mass Residual"), y = "Mass (ktons/yr)")
   # if(printfigs==T){ ggsave(filename = file.path(file_dir,nodes[i],paste0("Grph Ann Resid",nodes[i]," ",scens,".png")), width = gage_widths[2],height = gage_heights[2])}
 
   cumsum_err <- cumsum(diff$Value) #get cumulative error
   cumsum_df <- diff
-  cumsum_df$Value = cumsum_err 
-  
+  cumsum_df$Value = cumsum_err
+
   #annual cumsum residual
   r2 <- cumsum_df %>%
+    mutate(Value = Value/1000) %>%
     ggplot(aes(x = Year, y = Value)) +
     geom_line() +
-    theme_light() + 
+    theme_light() +
     scale_y_continuous(labels = scales::comma) +
-    geom_hline(yintercept = 0) + 
+    geom_hline(yintercept = 0) +
     theme(legend.position = "none", axis.title.x = element_blank()) +
-    
-    labs(title = paste(nodes[i],"Annual Cummulative Mass Residual"), y = "Mass (tons/yr)")
+
+    labs(title = paste(nodes[i],"Annual Cummulative Mass Residual"), y = "Mass (ktons/yr)")
   # if(printfigs==T){ ggsave(filename = file.path(ofigs,nodes[i],paste0("Grph Ann Cumsum Resid",nodes[i]," ",scens,".png")), width = gage_widths[2],height = gage_heights[2])}
-  
+
   grid.arrange(r1,r2,ncol=1)
-  
+
   #annual metrics
   mae <- round(sum(abs(diff$Value))/length(diff$Value))
   bias <- round(sum(diff$Value)/length(diff$Value))
   error_perc <- round(mae/mean(gage$Value)*100)
   ann_avgmass <- round(mean(gage$Value))
   # print(paste(title,"mae",mae,"bias",bias,"Error % of avg mass",error_perc*100))
-  
-  #flow for stats 
+
+  #flow for stats
   gage <- df_annual %>%
     dplyr::filter(Variable == flownm[i] & DataType == "Obs")
-  
+
   simulated <- df_annual %>%
     dplyr::filter(Variable == flownm[i] & DataType == "Sim")
   diff <- gage
@@ -216,29 +230,66 @@ for (i in 1:length(nodes)) {
   mae_flow <- round(sum(abs(diff$Value))/length(diff$Value))
   bias_flow <- round(sum(diff$Value)/length(diff$Value))
   error_perc_flow <- round(mae_flow/mean(gage$Value)*100)
-  ann_avgflow <- round(mean(gage$Value))  
-  
+  ann_avgflow <- round(mean(gage$Value))
 
   #create a sperate matrix of annual stats to store the % of gage erorr
   if(!exists("annstats") | i==1){
-    annstats <- array(c(nodes[i],mae,bias,ann_avgmass,error_perc,mae_flow,bias_flow,ann_avgflow,error_perc_flow)) 
+    annstats <- array(c(nodes[i],mae,bias,ann_avgmass,error_perc,mae_flow,bias_flow,ann_avgflow,error_perc_flow))
   } else {
     annstats <- rbind(annstats,c(nodes[i],mae,bias,ann_avgmass,error_perc,mae_flow,bias_flow,ann_avgflow,error_perc_flow))
   }
 
-    #make row names then don't print reach
-    if (dim(annstats)[1]==length(nodes)) { #this should mean you ran all of the traces
-      colnames(annstats) <- c("Reach","MAE Mass","Bias Mass","Avg Mass","Error % of Avg Mass","MAE Flow","Bias Flow","Avg Flow","Error % of Avg Flow")
-      rownames(annstats) <- nodes
-      write.csv(annstats[,2:dim(annstats)[2]],file = file.path(file_dir,paste0("AnnualVerificationStats",scens,".csv")))
-    } else {
-      write.csv(annstats,file = file.path(file_dir,paste0("Partial_AnnualVerificationStats",scens,".csv")))
-    }
+  #make row names then don't print reach
+  if (dim(annstats)[1]==length(nodes)) { #this should mean you ran all of the traces
+    colnames(annstats) <- c("Reach","MAE Mass","Bias Mass","Avg Mass","Error % of Avg Mass","MAE Flow","Bias Flow","Avg Flow","Error % of Avg Flow")
+    rownames(annstats) <- nodes
+    write.csv(annstats[,2:dim(annstats)[2]],file = file.path(file_dir,paste0("AnnualVerificationStats",scens,".csv")))
+  } else {
+    write.csv(annstats,file = file.path(file_dir,paste0("Partial_AnnualVerificationStats",scens,".csv")))
+  }
+
+  #### monthly #####
+  
+  # 3 panel monthly plot
+  p1 <- df_monthly %>%
+    dplyr::filter(Variable == flownm[i])  %>%
+    group_by(DataType,Variable,Date) %>%
+    mutate(Value = Value/1000) %>%
+    ggplot(aes(x = Date, y = Value, color = DataType)) +
+    geom_line() +
+    theme_light() + 
+    theme(axis.title.x = element_blank()) +
+    labs(title = paste(nodes[i]), y = "Flow (KAF/mo)",x="")
+  # print(p1)
+  
+  p2 <- df_monthly %>%
+    dplyr::filter(Variable == massnm[i])  %>%
+    group_by(DataType,Variable,Date) %>%
+    mutate(Value = Value/1000) %>%
+    ggplot(aes(x = Date, y = Value, color = DataType)) +
+    geom_line() +
+    theme_light() +
+    theme(axis.title.x = element_blank()) +
+    labs(y = "Mass (ktons/mo)",x="")
+  # print(p2)
+
+  p3 <- df_monthly %>%
+    dplyr::filter(Variable == concnm[i])  %>%
+    group_by(DataType,Variable,Date) %>%
+    ggplot(aes(x = Date, y = Value, color = DataType)) +
+    geom_line() +
+    theme_light() +
+    theme(axis.title.x = element_blank()) +
+    labs(y = "Concentration (mg/l)",x="")
+  # print(p3)
+  
+  grid.arrange(p1,p2,p3,ncol=1)
+  
+  #### end monthly ####
 
   } #end node loop
   
 dev.off() #mega plot
-
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ## 5. Plot Mass Balance 
